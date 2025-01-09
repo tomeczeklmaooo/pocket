9be53dc514a0db95790681fbd8c33618c41c4fc9
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-01-09 16:14:43 +0100

    current date when adding entries

--- src/transaction.c
+++ src/transaction.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "include/transaction.h"
 #include "include/file.h"
@@ -6,7 +7,18 @@
 void transaction(char mode, float amount, char place[32])
 {
 	char line[512];
-	snprintf(line, sizeof(line), "%c;DD/MM/YYYY;%.2f;%s\n", mode, amount, place);
+	time_t t = time(NULL);
+	struct tm *local_time = localtime(&t);
+	snprintf(
+		line,
+		sizeof(line),
+		"%c;%02d/%02d/%04d;%.2f;%s\n",
+		mode,
+		local_time->tm_mday,
+		local_time->tm_mon + 1,
+		local_time->tm_year + 1900,
+		amount,
+		place);
 
 	write_file("test.txt", line, 0);
 }
